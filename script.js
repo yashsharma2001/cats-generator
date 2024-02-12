22c56cccebb6a8fd28565d3ef6c29e5e7ae6de5f
Yash Sharma <105627588+yashsharma2001@users.noreply.github.com>
2024-02-12 21:50:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const generateBtn = document.getElementById("generate-btn");
+const catImage = document.getElementById("cat-image");
+const catFact = document.getElementById("cat-fact");
+
+function generateCat() {
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((response) => response.json())
+    .then((data) => {
+      const imageUrl = data[0].url;
+      catImage.src = imageUrl;
+    });
+
+  fetch("https://cat-fact.herokuapp.com/facts/random")
+    .then((response) => response.json())
+    .then((data) => {
+      const factText = data.text;
+      catFact.textContent = factText;
+    });
+}
+
+generateBtn.addEventListener("click", generateCat);
+
+document.addEventListener("DOMContentLoaded", function () {
+  generateCat();
+});
